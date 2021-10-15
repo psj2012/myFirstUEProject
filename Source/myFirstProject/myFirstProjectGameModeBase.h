14201bb2004242069cff8c4eb2f34f048fb2cdef
psj2012 <30601056+psj2012@users.noreply.github.com>
2021-10-15 22:54:47 +0100

    Add files via upload

---
+++ Source/myFirstProject/myFirstProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "myFirstProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYFIRSTPROJECT_API AmyFirstProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
